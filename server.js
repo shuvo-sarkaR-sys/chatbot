c415a8b4140ea6d58d7422047724c6c38ed0a5a1
shuvo-sarkaR-sys <md.sirforce@gmail.com>
2025-03-02 15:56:50 +0600

    updated

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors")
+ 
+
+const app = express();
+app.use(cors({origin: "http://localhost:5173"}));
+app.use(express.json());
+
+app.post("/chatbot", (req, res) => {
+  const { message } = req.body;
+  
+  let reply = "I'm just a simple bot. Ask me something else!";
+  if (message.toLowerCase().includes("hello")) reply = "Hello! How can I help you?";
+  if (message.toLowerCase().includes("bye")) reply = "Goodbye! Have a great day!";
+  if (message.toLowerCase().includes("location")) reply = "2 street, los angles, USA";
+  if(message.toLowerCase().includes("service")) reply = "Digital marketing, web development, E-commerce, shopify etc."
+  if(message.toLowerCase().includes('create you')) reply= "Shuvo Sarkar from bangladesh"
+  res.json({ reply });
+});
+
+app.listen(5000, () => console.log("Chatbot backend running on port 5000"));
